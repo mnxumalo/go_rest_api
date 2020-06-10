9b19f724de3d7c58273bc7ec58ffe51c9a22a07f
Mthulisi Nxumalo <mnxumalo@gmail.com>
2020-06-10 11:11:20 +0200

    Go REST API example

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works and get to try it out.",
+	},
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to Events API v1!")
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+
+	json.NewEncoder(w).Encode(events)
+
+}
+
+func createEvent(w http.ResponseWriter, r *http.Request) {
+
+	var newEvent event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprint(w, "Please enter events data.")
+	}
+
+	json.Unmarshal(reqBody, &newEvent)
+
+	events = append(events, newEvent)
+
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+}
+
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+
+	eventID := mux.Vars(r)["id"]
+
+	for _, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	var updatedEvent event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprint(w, "Please enter the event data to update.")
+	}
+
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			singleEvent.Description = updatedEvent.Description
+			singleEvent.Title = updatedEvent.Title
+			events = append(events[:i], singleEvent)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID %v has been deleted successfully.", eventID)
+		}
+	}
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", homeLink)
+
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
+
+	router.HandleFunc("/event", createEvent).Methods("POST")
+
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
